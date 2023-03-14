1508f220cf6749d3a5a62fbd328d1b0e59623623
gitsaac <127430566+gitsaac@users.noreply.github.com>
2023-03-13 20:35:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,28 +43,18 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice)
 
-
-
-
-#streamlit.header("Fruityvice Fruit Advice!")
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#streamlit.dataframe(fruityvice_normalized)
-
+streamlit.header("The Fruit load list contains:")
+def get_fruit_load_list ():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+if streamlit.button(get_fruit_load_list):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+    
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-#               SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit load list contains:")
-               #text("Hello from Snowflake:")
-streamlit.dataframe(my_data_rows)
-#text(my_data_row)
 
 streamlit.text("¿What fruit would you like to add?")
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
